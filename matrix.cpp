cde22d577235bf2a67aa1eee37637665ea685162
Leisan <allarepossible@gmail.com>
2019-11-14 18:10:13 +0300

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -11,6 +11,10 @@ private:
 public:
     friend istream& operator>> (istream& in, DMatrix& matrix);
     friend ostream& operator<< (ostream& out, const DMatrix& matrix);
+    friend DMatrix operator+ (const DMatrix& m1, const DMatrix& m2);
+    friend DMatrix operator* (const DMatrix& el, int a);
+    friend DMatrix operator* (double p, const DMatrix& el) {return el*p;}
+    
     void f (int m, int n) {
         m_d = new double* [m];
         for (int i = 0; i < m; i++) {
@@ -33,9 +37,10 @@ public:
         }
         delete[] m_d;
     }
-    DMatrix(const DMatrix &ob){
+
+    DMatrix(const DMatrix &ob){      // this
         f(ob.m_row,ob.m_column);
-        
+
         for(int i = 0;i < ob.m_row; i++){
             for(int j = 0; j<ob.m_column; j++)
             {
@@ -45,7 +50,7 @@ public:
     }
     DMatrix& operator=(const DMatrix &ob){
         f(ob.m_row,ob.m_column);
-        
+
         for(int i = 0;i < ob.m_row; i++){
             for(int j = 0; j<ob.m_column; j++)
             {
@@ -54,36 +59,49 @@ public:
         }
         return *this;
     }
-    
-    DMatrix operator*(int a){
-        DMatrix pr(this->m_row, this->m_column);
-        for(int i = 0; i < this->m_row; i++){
-            for(int j = 0; j < this->m_column; j++)
-            {
-                pr.m_d[i][j]=this->m_d[i][j]*a;
-            }
-        }
-        return pr;
-    }
-    
-    double& operator()(int a, int b){
-        return this->m_d[a][b];
+
+    double& operator()(int row, int col){
+        return m_d[row][col];
     }
 };
+
+DMatrix operator*(const DMatrix& el, int a) {
+    DMatrix pr(el.m_row, el.m_column);
+    for(int i = 0; i < el.m_row; i++){
+        for(int j = 0; j < el.m_column; j++)
+        {
+            pr.m_d[i][j]=el.m_d[i][j]*a;
+        }
+    }
+    return pr;
+}
+
+DMatrix operator+ (const DMatrix& m1, const DMatrix& m2) {
+    DMatrix sum(m1.m_row, m1.m_column);
+    
+    for(int i = 0; i < m1.m_row; i++){
+        for(int j = 0; j < m1.m_column; j++)
+        {
+            sum.m_d[i][j] = m1.m_d[i][j] + m2.m_d[i][j];
+        }
+    }
+    
+    return sum;
+}
 
 istream& operator>> (istream& in, DMatrix& matrix) {
     cout << "Введите количество строк и столбцов: ";
     in >> matrix.m_row >> matrix.m_column;
     matrix.f(matrix.m_row, matrix.m_column);
     cout << "Введите " << matrix.m_row * matrix.m_column << " элементов" << endl;
-    
+
     for(int i = 0; i< matrix.m_row; i++){
         for(int j = 0; j<matrix.m_column; j++)
         {
             in >> matrix.m_d[i][j];
         }
     }
-    
+
     return in;
 }
 
@@ -98,28 +116,29 @@ ostream& operator<< (ostream& out, const DMatrix& matrix) {
         out << endl;
     }
     out << endl;
-    
+
     return out;
 }
 
+
 int main()
 {
-    DMatrix matrix;
-    cout << endl;
-    cin >> matrix;
-    cout << endl;
-    cout << matrix<< endl;
-    cout << endl;
-    DMatrix matrix2(matrix);
+    DMatrix V, U;
+    double x;
+    cin >> x;
+    cin >> V;
+    cin >> U;
+
+    DMatrix Y = x * V + 2 * U;
+
+    cout << endl << endl;
+    cout << Y;
+
+    DMatrix Z = V(1,2) * U + U(2,1) * V;
+
+    cout << Z;
     
-    cout << matrix2*2<< endl;
-    
-    DMatrix matrix3 = matrix2*10;
-    
-    
-    cout << matrix3<< endl;
-    
-    cout << matrix2(1,2)<< endl;
     return 0;
-    
+
 }
+
